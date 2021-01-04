1c087e03a4f40ec312c8ebb09002014c07b9883c
Ron Dagdag <3879302+rondagdag@users.noreply.github.com>
2021-01-03 19:51:24 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
-
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_OBJECT_TRACKING)
-
+basic.showIcon(IconNames.Heart)
 basic.forever(function () {
     huskylens.request()
     if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
